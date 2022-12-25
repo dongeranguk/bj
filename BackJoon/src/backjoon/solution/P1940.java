04e034ffe8d544d81214b5c28500b4c2bb52c6fd
dongeranguk <rooetym@naver.com>
2022-12-25 22:56:42 +0900

    투 포인터 알고리즘을 사용하여 어떠한 수를 두 수의 합으로 나타낼 수 있는 경우의 수 구하기

---
+++ BackJoon/src/backjoon/solution/P1940.java
@@ -0,0 +1,41 @@
+package backjoon.solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P1940 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] A = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			
+			A[i] = Integer.parseInt(st.nextToken());
+			
+		}
+		Arrays.sort(A);
+		
+		int i = 0;
+		int j = A.length-1;
+		int count = 0;
+		while(i < j) {
+			if(A[i]+A[j] < M) { 
+				i++;
+			} else if(A[i] + A[j] > M) {
+				j--;
+			} else {
+				count++;
+				i++; j--;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
