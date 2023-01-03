ecd80eaf55953f8913c0d637c46e684bfcfaa979
dongeranguk <rooetym@naver.com>
2023-01-03 23:33:30 +0900

    슬라이딩 윈도우 알고리즘으로 DNA 문자열 배열에서 유효한 비밀번호 구하기2

--- BackJoon/src/backjoon/solution/P12891.java
+++ BackJoon/src/backjoon/solution/P12891.java
@@ -6,39 +6,98 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P12891 {
+	static int[] myArr;
+	static int[] checkArr;
+	static int checkSecret;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int S = Integer.parseInt(st.nextToken());
 		int P = Integer.parseInt(st.nextToken());
 		
-		char[] A = new char[S];
-		
-		String str_A = br.readLine();
-		
-		A = str_A.toCharArray();
-		
-		char[] checkArr = new char[4];
-		char[] myArr = new char[4];
-		
+		String str = br.readLine();
+		char[] A = str.toCharArray();
+
+		myArr = new int[4];
+		checkArr = new int[4];
 		int Result = 0;
-		int checkSecret = 0;
 		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<4; i++) {
+			checkArr[i] = Integer.parseInt(st.nextToken());
+			if(checkArr[i] == 0)
+				checkSecret++;
+		}
+
 		for(int i=0; i<P; i++) {
-			System.out.println((char)A[i] - 48);
-			System.out.println(A[i] - 65);
-			if((A[i] - 'A') == 'A') {
-				myArr[0]++;
-			} else if((A[i] - 'A') == 'C') {
-				myArr[1]++;
-			} else if((A[i] - 'A') == 'G') {
-				myArr[2]++;
-			} else if((A[i] - 'A') == 'T') {
-				myArr[3]++;
-			}
+			add(A[i]);
+		}
+
+		if(checkSecret == 4) 
+			Result++;
+
+
+		for(int i=P; i<S; i++) {
+			int j = i - P;
+			add(A[i]);
+			remove(A[j]);
+			if(checkSecret == 4) 
+				Result++;
 		}
 		
+		
+		System.out.println(Result);
+	}
+	
+	public static void add(char c) {
+		switch(c) {
+			case 'A' :
+				myArr[0]++;
+				if(myArr[0] == checkArr[0])
+					checkSecret++;
+				break;
+			case 'C' :
+				myArr[1]++;
+				if(myArr[1] == checkArr[1])
+					checkSecret++;
+				break;
+			case 'G' :
+				myArr[2]++;
+				if(myArr[2] == checkArr[2])
+					checkSecret++;
+				break;
+			case 'T' :
+				myArr[3]++;
+				if(myArr[3] == checkArr[3])
+					checkSecret++;
+				break;
+		}
+	}
+	
+	public static void remove(char c) {
+		switch(c) {
+			case 'A' :
+				if(myArr[0] == checkArr[0])
+					checkSecret--;
+				myArr[0]--;
+				break;
+			case 'C' :
+				if(myArr[1] == checkArr[1])
+					checkSecret--;
+				myArr[1]--;
+				break;
+			case 'G' :
+				if(myArr[2] == checkArr[2])
+					checkSecret--;
+				myArr[2]--;
+				break;
+			case 'T' :
+				if(myArr[3] == checkArr[3])
+					checkSecret--;
+				myArr[3]--;
+				break;
+		}
 	}
 }
